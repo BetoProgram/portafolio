94d259e9c79beaae74ebcdc034086bb581e358cc
BetoProgram <juancho34_8@gmail.com>
2017-11-19 12:41:52 -0600

    Rutas en el menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent,HomeComponent,ProductComponent } from './components/index.pages';
 
 const APP_ROUTES: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent }, 
     { path: 'product', component: ProductComponent }, 
-    { path: '**', pathMatch:'full',redirectTo:'' },
+    { path: '**', pathMatch:'full',redirectTo:'home' },
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES,{useHash:true});
