23731dc46b5dd06d905e31f28d629a8dd1cc64d9
BetoProgram <juancho34_8@gmail.com>
2017-11-20 21:49:41 -0600

    se crea metodo que trae la data de firebase en servicio

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,13 +5,27 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
   public info:any={};
+  public equipo:any[]=[];
   public cargada:boolean = false;
+  public cargada_sobre:boolean = false;
 
   constructor(public http:Http) { 
+    this.carga_info();
+    this.carga_sobre_nostros();
+  }
+  public carga_info(){
     this.http.get("assets/data/info.page.json")
     .subscribe(data=>{
       this.cargada = true;
       this.info=data.json();
+    })
+  }
+
+  public carga_sobre_nostros(){
+    this.http.get("https://portafolio-5ea28.firebaseio.com/equipo.json")
+    .subscribe(data=>{
+      this.cargada_sobre = true;
+      this.equipo=data.json();
     })
   }
 
